cd5c4cd8d0c149975730669c22534d910c0e5d7e
Garv <garvmalik11@gmail.com>
2023-07-27 16:56:01 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+// let elamclass = document.getElementsByClassName('footer');
+// // console.log(elamclass);
+// // elamclass[0].style.background = "yellow";
+
+
+// let arr = document.getElementsByTagName('button');
+// console.log(arr);
+// var createdElement = document.createElement('p')
+// createdElement.innerText = "HI ";
+// arr[0].appendChild(createdElement);
+
+// var createdElement2 = document.createElement('H1');
+// createdElement2.innerText="QWERTY YTREWQ";
+// arr[0].appendChild(createdElement2);
+// arr[0].replaceChild( createdElement2,createdElement,);
+
+
+
+
